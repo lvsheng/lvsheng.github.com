189da1615f30ebc76fd94afaae32fa4f4e333249
lvsheng <lvsheng@baidu.com>
2014-11-06 14:37:32 +0800

    基本架子

---
+++ src/gameScene/zIndexConf.js
@@ -0,0 +1,10 @@
+/**
+ */
+(function (namespace) {
+    namespace.zIndexConf = {
+        hitBeing: 0, //被打者：大叔和情侣
+        holeMask: 10, //洞的前挡板部分，挡住下面的大叔和情侣
+        effectProp: 20, //道具：心、雾、+1、台词等
+        hammer: 30
+    };
+})(window.myGame);
