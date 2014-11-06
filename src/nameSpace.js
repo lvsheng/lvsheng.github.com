458d446bcb8c0bdd3ea3758aabf98cfb0f3d7333
lvsheng <lvsheng@baidu.com>
2014-11-06 13:54:49 +0800

    基本架子

---
+++ src/nameSpace.js
@@ -0,0 +1,3 @@
+(function (exportTo) {
+    exportTo.myGame = {};
+})(window);
---
+++ src/resourceFileMap.js
@@ -0,0 +1,16 @@
+(function (namespace) {
+    namespace.resourceFileMap = {
+        background_png: 'res/background.png',
+        fog_png: 'res/fog.png',
+        hammer_png: 'res/hammer.png',
+        heartLeft_png: 'res/heartLeft.png',
+        heartRight_png: 'res/heartRight.png',
+        holeFront_png: 'res/holeFront.png',
+        lover_png: 'res/lover.png',
+        loverSay_png: 'res/loverSay.png',
+        peng_png: 'res/peng.png',
+        plus_png: 'res/plus.png',
+        uncle_png: 'res/uncle.png',
+        uncleSay_png: 'res/uncleSay.png'
+    }
+})(window.myGame);
